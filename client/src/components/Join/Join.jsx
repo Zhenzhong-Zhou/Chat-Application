ddb86333c20d1428e9990484fc4a7c0836d807ff
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-23 19:05:51 -0600

    Add join component into app

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,6 +1,22 @@
+import {useState} from "react";
+import {Link} from "react-router-dom";
+import "./join.css";
+
 const Join = () => {
+	const [username, setUsername] = useState("");
+	const [room, setRoom] = useState("");
+
 	return (
-		<div>Join</div>
+		<div className={"joinOuterContainer"}>
+			<div className={"joinInnerContainer"}>
+				<h1 className={"heading"}>Join</h1>
+				<div><input placeholder={"Username"} className={"joinInput"} type={"text"} onChange={event => setUsername(event.target.value)}/></div>
+				<div><input placeholder={"Room"} className={"joinInput mt-20"} type={"text"} onChange={event => setRoom(event.target.value)}/></div>
+				<Link onClick={event => (!username || !room) ? event.preventDefault() : null} to={`/chat?name=${username}&room=${room}`}>
+					<button className={"button mt-20"} type={"submit"}>Sign In</button>
+				</Link>
+			</div>
+		</div>
 	);
 };
 
