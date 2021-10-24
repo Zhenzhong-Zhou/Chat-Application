2624495b568b02800b7932cdbf887a59b9fb3ba3
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-10-23 21:34:15 -0600

    Chat component connects with socket.io

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,4 +1,23 @@
-const Chat = () => {
+import {useEffect, useState} from "react";
+import queryString from "query-string";
+import "./chat.css";
+import {io} from "socket.io-client";
+
+let socket;
+
+const Chat = ({location}) => {
+	const [username, setUsername] = useState("");
+	const [room, setRoom] = useState("");
+	const ENDPOINT = process.env.REACT_APP_ENDPOINT;
+
+	useEffect(() => {
+		const {username, room} = queryString.parse(location.search);
+		socket = io(ENDPOINT);
+		setUsername(username);
+		setRoom(room);
+		socket.emit("join", {username, room});
+	}, [location.search, ENDPOINT]);
+
 	return (
 		<div>Chat</div>
 	);
